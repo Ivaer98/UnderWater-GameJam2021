c7c010a3e9c9ecdedab73a34ef640388638e516b
Ivan Enrique Elias Ramirez <Ivan._.elias@hotmail.com>
2021-01-31 15:33:18 -0400

    Added A lot of Things

---
+++ Assets/Scripts/CollectableCoin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableCoin : MonoBehaviour
+{
+    [SerializeField] int timeToDestroy = 6;
+    [SerializeField] int scoreToAdd = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Destroy(gameObject, timeToDestroy);        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+        ScoreBoard scoreBoard = FindObjectOfType<ScoreBoard>();
+        scoreBoard.AddScore(scoreToAdd);
+        Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/CollectableHeart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableHeart : MonoBehaviour
+{
+    [SerializeField] int timeToDestroy = 10;
+    [SerializeField] int healthToRestore = 150;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerPlaneMovement playerPlane = FindObjectOfType<PlayerPlaneMovement>();
+            playerPlane.health += healthToRestore;
+            if (playerPlane.health > playerPlane.maxHealth)
+            {
+            playerPlane.health = playerPlane.maxHealth;
+
+            }
+            
+            Destroy(gameObject);
+        }
+
+    }*/
+}
