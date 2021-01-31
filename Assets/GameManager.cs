22dd151a1eddb19cc4bf5cb0a92648c3307c3a38
Ivan Enrique Elias Ramirez <Ivan._.elias@hotmail.com>
2021-01-31 14:19:10 -0400

    Added GGJ Splash And GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] bool resetHighScore = false;
+    // This Script is called only the first time that runing
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    private void Start()
+    {
+        if (resetHighScore)
+        {
+            PlayerPrefs.DeleteKey("HighScore");
+        }
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    /* Update is called once per frame
+    void Update()
+    {
+
+    }*/
+    public void LoadMainMenu()
+    {
+        FindObjectOfType<ScoreBoard>().ResetScore();
+        SceneManager.LoadScene(0);
+
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("NaziInvation");
+        FindObjectOfType<ScoreBoard>().ResetScore();
+    }
+    public IEnumerator LoadGameOverAftherSeconds()
+    {
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("GameOver");
+
+    }
+    public void LoadGameOver()
+    {
+        StartCoroutine(LoadGameOverAftherSeconds());
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
---
+++ Assets/ScoreBoard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    int scoreValue;
+
+    private void Awake()
+    {
+        //Singleton Paternon
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = scoreValue.ToString();
+    }
+
+    //ScoreBoard Functionality
+
+    public int GetScore()
+    {
+        return scoreValue;
+    }
+
+    public void AddScore(int score)
+    {
+        scoreValue += score;
+    }
+    public void ResetScore()
+    {
+        Destroy(gameObject);
+    }
+}
