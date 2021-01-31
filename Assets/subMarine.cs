f6f5ab88097b7ced8b861d6a011d76725315437b
Ivan Enrique Elias Ramirez <Ivan._.elias@hotmail.com>
2021-01-30 20:58:18 -0400

    Better movement, added lights

--- Assets/subMarine.cs
+++ Assets/subMarine.cs
@@ -7,8 +7,8 @@ public class subMarine : MonoBehaviour
 {
 
     [SerializeField] ParticleSystem bubbles;
-    [SerializeField] float pushForce = 20000;
-    [SerializeField] float turnForce = 50;
+    [SerializeField] float pushForce = 200;
+    [SerializeField] float turnForce = 100;
     [SerializeField] GameObject light1;
     [SerializeField] GameObject light2;
     [SerializeField] GameObject explosion;
@@ -24,36 +24,61 @@ public class subMarine : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        bubbles.Play();
+
+    }
+
+    private void TurnLightOff()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
         light1.SetActive(false);
         light2.SetActive(false);
-        bubbles.Stop();
-
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+        light1.SetActive(true);
+        light2.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         SwinMovement();
+        TurnLightOff();
     }
 
     private void SwinMovement()
     {
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.A))
         {
            rb.angularVelocity = Vector3.zero;
             transform.Rotate(Vector3.left * turnForce * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.D))
         {
             rb.angularVelocity = Vector3.zero;
             transform.Rotate(Vector3.right * turnForce * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.W))
         {
+
             rb.AddRelativeForce(Vector3.forward * pushForce * Time.deltaTime);
-            bubbles.Play();
         }
+        if (Input.GetKey(KeyCode.Q))
+        {
+
+            rb.AddRelativeForce(Vector3.down * pushForce * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+
+            rb.AddRelativeForce(Vector3.up * pushForce * Time.deltaTime);
+        }
+
+
     }
     private void OnCollisionEnter(Collision collision)
     {
